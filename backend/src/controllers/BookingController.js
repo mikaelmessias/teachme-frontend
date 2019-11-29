6577acd16c850b669f8dcbbcfef2924590d6b31e
Mikael Messias <mikaelmessias@outlook.com>
2019-11-29 15:58:26 -0300

    Update BookingController
    Signed-off-by: Mikael Messias <mikaelmessias@outlook.com>

--- backend/src/controllers/BookingController.js
+++ backend/src/controllers/BookingController.js
@@ -1,26 +1,41 @@
 const Booking = require('../models/Booking');
+const User = require('../models/User');
+const Mentor = require('../models/Mentor');
 
 module.exports = {
   async store(req, res) {
-    const { date } = req.body;
-    const { duration } = req.body;
-    const { tech } = req.body;
-    const { user } = req.body;
-    const { mentor } = req.body;
-    const { status } = req.body;
+    const { date, hour, duration, tech, status } = req.body;
+    const { user_id } = req.headers;
+    const { mentor_id } = req.params;
 
-    let booking = await Booking.findOne({ date, user, mentor });
+    const user = await User.findById(user_id);
+    const mentor = await Mentor.findById(mentor_id);
 
-    if(!booking) {
-      booking = await Booking.create({
-        date,
-        duration,
-        tech,
-        user,
-        mentor,
-        status
-      });
+    if(!user) {
+      return res.status(400).json({ error: 'User does not exists' });
     }
+
+    if(!mentor) {
+      return res.status(400).json({ error: 'Mentor does not exists '});
+    }
+
+    let booking = await Booking.findOne({ date, hour, mentor: mentor_id });
+
+    if(booking) {
+      return res.status(409).json({ error: 'There is a booked attendance for the same date and menthor' });
+    }
+
+    booking = await Booking.create({
+      date,
+      hour,
+      duration,
+      tech,
+      user: user_id,
+      mentor: mentor_id,
+      status
+    });
+
+    await booking.populate('tech').populate('user').populate('mentor').execPopulate();
 
     return res.json(booking);
   }
