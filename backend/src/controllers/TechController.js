607932ba2233d584b3fef7f30c7c3435b0687f7f
Mikael Messias <mikaelmessias@outlook.com>
2019-12-01 13:30:49 -0300

    Refactor code from TechController
    Signed-off-by: Mikael Messias <mikaelmessias@outlook.com>

--- backend/src/controllers/TechController.js
+++ backend/src/controllers/TechController.js
@@ -6,6 +6,12 @@ module.exports = {
 
     const techs = await Tech.find({ description: tech });
 
+    if(techs.length == 0) {
+      return res.status(404).json({
+        error: "No tech found with given keyword"
+      })
+    }
+
     return res.json(techs);
   },
 
@@ -15,12 +21,16 @@ module.exports = {
 
     let tech = await Tech.findOne({ description });
 
-    if(!tech) {
-      tech = await Tech.create({
-        description,
-        thumbnail: filename
-      });
+    if(tech) {
+      return res.status(400).json({
+        error: "Tech already registered"
+      })
     }
+
+    tech = await Tech.create({
+      description,
+      thumbnail: filename
+    });
 
     return res.json(tech);
   }
