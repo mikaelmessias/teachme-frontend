5c673a5a76b91f2bb8e144dc7fcce9c048979a25
Mikael Messias <mikaelmessias@outlook.com>
2019-12-06 10:42:22 -0300

    Refactor code from backend/src/routes.js
    Signed-off-by: Mikael Messias <mikaelmessias@outlook.com>

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -22,21 +22,20 @@ routes.get('/techs', TechController.index);
 routes.post('/techs', thumbUpload.single('thumbnail'), TechController.store);
 
 routes.post('/users', avatarUpload.single('avatar'), UserController.store);
-
 routes.post('/mentors', avatarUpload.single('avatar'), MentorController.store);
 
-routes.post('/users/authenticate', UserController.authenticate);
+routes.post('/authenticate', UserController.authenticate);
 
 routes.use(authMiddleware);
 
 routes.get('/users', UserController.show);
-routes.get('/users/search', MentorController.index);
+routes.get('/search', MentorController.index);
 
-routes.get('/mentors/bookings', BookingController.index);
-routes.post('/mentors/bookings', BookingController.store);
-routes.put('/mentors/bookings/:booking', BookingController.update);
+routes.get('/bookings', BookingController.index);
+routes.post('/bookings', BookingController.store);
+routes.put('/bookings/:booking', BookingController.update);
 
-routes.put('/users/edit', avatarUpload.single('avatar'), UserController.update);
+routes.put('/users', avatarUpload.single('avatar'), UserController.update);
 
 routes.get('/mentors', MentorController.show);
 
